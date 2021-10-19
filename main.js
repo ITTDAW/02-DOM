d83c3f1924b80859cb40e4fdf1fa538a241482ff
JMTE <JMTEdeveloper@outlook.es>
2021-10-19 18:00:45 +0200

    Se añaden divs y se hace algun estilo css

--- main.js
+++ main.js
@@ -1,83 +1,127 @@
 
 function cargar(){
 
-
-
+    let contenedor=document.querySelector(".contenedor");
+    
+    
+   
+    
     let divformulario=document.createElement("div");
     divformulario.id="divformulario";
-    
-    let formulario = document.createElement("form");//<p></p>
-    formulario.id = "formulario" //<p id="parrafo1"></p>
+    let formulario = document.createElement("form");
+    formulario.id = "formulario" ;
+    formulario.action="#";
+    formulario.method="post";
+
 
     let fieldset= document.createElement("fieldset");
 
     let legend= document.createElement("legend");
-    legend.textContent="Conocimientos Programación"
+    legend.textContent="Ficha personal alumno"
     
 
+   
     
     
-    // Crear nodo de tipo Text
 
     let textos=document.createElement("div");
     textos.id="textos";
 
+    let labeldni=document.createElement("label");
+    labeldni.textContent="DNI ALUMNO"
     let inputdni = document.createElement("input");
     inputdni.id="inputdni";
     inputdni.type="text";
     inputdni.placeholder="Introduce aqui tu DNI";
 
+    let labelnombre=document.createElement("label");
+    labelnombre.textContent="NOMBRE"
     let inputnombre = document.createElement("input");
     inputnombre.id="inputnombre";
     inputdni.type="text";
     inputnombre.placeholder="Introduce aqui tu Nombre";
 
+    let labelapellidos=document.createElement("label");
+    labelapellidos.textContent="APELLIDOS"
     let inputapellidos = document.createElement("input");
     inputapellidos.id="inputapellidos";
     inputdni.type="text";
     inputapellidos.placeholder="Introduce aqui tus apellidos";
 
+    let labeldireccion=document.createElement("label");
+    labeldireccion.textContent="DIRECCIÓN"
     let inputdireccion = document.createElement("input");
     inputdireccion.id="inputdireccion";
     inputdni.type="text";
     inputdireccion.placeholder="Introduce aqui tu direccion";
 
+    let labeltelefono=document.createElement("label");
+    labeltelefono.textContent="TELÉFONO"
     let inputtelefono = document.createElement("input");
     inputtelefono.id="inputdireccion";
     inputtelefono.type="number";
     inputtelefono.placeholder="Introduce aqui tu telefono";
 
-     
+    textos.appendChild(labeldni);
+    textos.appendChild(inputdni);//
+    textos.appendChild(labelnombre);
+    textos.appendChild(inputnombre);//
+    textos.appendChild(labelapellidos);
+    textos.appendChild(inputapellidos);//
+    textos.appendChild(labeldireccion);
+    textos.appendChild(inputdireccion);//
+    textos.appendChild(labeltelefono);
+    textos.appendChild(inputtelefono);//
+
+    let radios =document.createElement("div");
+    radios.id="radios";
+
     let gruporadio1 =document.createElement("div");
     gruporadio1.id="gruporadio1";
 
+    let labelgrupo1=document.createElement("label");
+    labelgrupo1.textContent="Nivel de Estudios";
+
+
+    let divbachiller =document.createElement("div")
     let labelradio1=document.createElement("label");
-    labelradio1.textContent="Radio 1"
+    labelradio1.textContent="Bachiller"
     let  radio1= document.createElement("input");
     radio1.id="radio1";
     radio1.type="radio";
     radio1.name="grupo1";
-
+    divbachiller.appendChild(labelradio1);
+    divbachiller.appendChild(radio1);
+    
+    let divciclomedio =document.createElement("div");
     let labelradio2=document.createElement("label");
-    labelradio2.textContent="Radio 2"
+    labelradio2.textContent="Ciclo Medio"
     let  radio2= document.createElement("input");
     radio2.id="radio2";
     radio2.type="radio";
     radio2.name="grupo1";
+    divciclomedio.appendChild(labelradio2);
+    divciclomedio.appendChild(radio2);
 
+    let divciclosuperior =document.createElement("div")
     let labelradio3=document.createElement("label");
-    labelradio3.textContent="Radio 3"
+    labelradio3.textContent="Ciclo Superior"
     let  radio3= document.createElement("input");
     radio3.id="radio3";
     radio3.type="radio";
     radio3.name="grupo1";
+    divciclosuperior.appendChild(labelradio3);
+    divciclosuperior.appendChild(radio3);
 
+    let divuniversidad =document.createElement("div")
     let labelradio4=document.createElement("label");
-    labelradio4.textContent="Radio 4"
+    labelradio4.textContent="Estudios Universitarios"
     let  radio4= document.createElement("input");
     radio4.id="radio4";
     radio4.type="radio";
     radio4.name="grupo1";
+    divuniversidad.appendChild(labelradio4);
+    divuniversidad.appendChild(radio4);
 
 
 
@@ -85,65 +129,73 @@ function cargar(){
     let gruporadio2 =document.createElement("div");
     gruporadio2.id="gruporadio2";
 
+    let labelgrupo2=document.createElement("label");
+    labelgrupo2.textContent="¿Cual es tu lenguaje de programación favorito?";
+
+    let divjava =document.createElement("div");
     let labelradio5=document.createElement("label");
-    labelradio5.textContent="Radio 5"
+    labelradio5.textContent="Java"
     let  radio5= document.createElement("input");
     radio5.id="radio5";
     radio5.type="radio";
     radio5.name="grupo2";
+    divjava.appendChild(labelradio5);
+    divjava.appendChild(radio5);
 
+    let divjavascript =document.createElement("div");
     let labelradio6=document.createElement("label");
-    labelradio6.textContent="Radio 6"
+    labelradio6.textContent="JavaScript"
     let  radio6= document.createElement("input");
     radio6.id="radio6";
     radio6.type="radio";
     radio6.name="grupo2";
+    divjavascript.appendChild(labelradio6);
+    divjavascript.appendChild(radio6);
 
+    let divcsharp =document.createElement("div");
     let labelradio7=document.createElement("label");
-    labelradio7.textContent="Radio 7"
+    labelradio7.textContent="C#"
     let  radio7= document.createElement("input");
     radio7.id="radio7";
     radio7.type="radio";
     radio7.name="grupo2";
+    divcsharp.appendChild(labelradio7);
+    divcsharp.appendChild(radio7);
 
+    let divpython =document.createElement("div");
     let labelradio8=document.createElement("label");
-    labelradio8.textContent="Radio 8"
+    labelradio8.textContent="Python"
     let  radio8= document.createElement("input");
     radio8.id="radio8";
     radio8.type="radio";
     radio8.name="grupo2";
+    divpython.appendChild(labelradio8);
+    divpython.appendChild(radio8);
 
 
     
     
-    // Añadir el nodo Text como hijo del nodo Element
-    textos.appendChild(inputdni);//
-    textos.appendChild(inputnombre);//
-    textos.appendChild(inputapellidos);//
-    textos.appendChild(inputdireccion);//
-    textos.appendChild(inputtelefono);//
+    
+    gruporadio1.appendChild(labelgrupo1);
+    gruporadio1.appendChild(divbachiller);
+    gruporadio1.appendChild(divciclomedio);
+    gruporadio1.appendChild(divciclosuperior);
+    gruporadio1.appendChild(divuniversidad);
 
-    gruporadio1.appendChild(labelradio1);
-    gruporadio1.appendChild(radio1);
-    gruporadio1.appendChild(labelradio2);
-    gruporadio1.appendChild(radio2);
-    gruporadio1.appendChild(labelradio3);
-    gruporadio1.appendChild(radio3);
-    gruporadio1.appendChild(labelradio4);
-    gruporadio1.appendChild(radio4);
 
-    gruporadio2.appendChild(labelradio5);
-    gruporadio2.appendChild(radio5);
-    gruporadio2.appendChild(labelradio6);
-    gruporadio2.appendChild(radio6);
-    gruporadio2.appendChild(labelradio7);
-    gruporadio2.appendChild(radio7);
-    gruporadio2.appendChild(labelradio8);
-    gruporadio2.appendChild(radio8);
+   
+    gruporadio2.appendChild(labelgrupo2);
+    gruporadio2.appendChild(divjava);
+    gruporadio2.appendChild(divjavascript);
+    gruporadio2.appendChild(divcsharp);
+    gruporadio2.appendChild(divpython);
 
+    radios.appendChild(gruporadio1);
+    radios.appendChild(gruporadio2);
 
     let divhobbies=document.createElement("div");
     divhobbies.id="divhobbies";
+
 
     let labelhobbies= document.createElement("label");
     labelhobbies.textContent="Selecciona tus hobbies"
@@ -273,8 +325,8 @@ function cargar(){
 
     fieldset.appendChild(legend);
     fieldset.appendChild(textos);
-    fieldset.appendChild(gruporadio1);
-    fieldset.appendChild(gruporadio2);
+    fieldset.appendChild(radios);
+    
     fieldset.appendChild(divhobbies);
     fieldset.appendChild(imagen1);
     fieldset.appendChild(imagen2);
@@ -288,8 +340,11 @@ function cargar(){
 
     
     divformulario.appendChild(formulario);
-    // Añadir el nodo Element como hijo de la pagina
-    document.body.appendChild(divformulario);
 
+    
+  
+    contenedor.appendChild(divformulario);
+
+    
 }
 
